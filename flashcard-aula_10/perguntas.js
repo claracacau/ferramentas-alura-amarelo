df6bf4c883d0c61b2cf2a2ce09559330898b096d
claracacau <159549472+claracacau@users.noreply.github.com>
2024-11-01 10:41:46 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'É um espaço de memória que armazena um valor'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os 5 oceanos do mundo?',
+    'Oceano Pacífico, Atlântico, Índico, Ártico e Antártico'
+)
+
+criaCartao(
+    'O que é programação',
+    'É o processo de criar instruções para que computadores executem tarefas específicas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual maior continente do mundo?',
+    'Ásia'
+)
